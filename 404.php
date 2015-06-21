cbc6d7e379339a2f5e7bc92e5324a1dc1241e2af
babobski <info@babobski.nl>
2015-06-21 21:35:16 +0200

    updated translation and translated 404
    updated translation and translated 404

--- 404.php
+++ 404.php
@@ -11,6 +11,6 @@
 ?>
 <?php BsWp::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
-<h2>Page not found</h2>
+<h2><?php echo __('Page not found', 'wp_babobski'); ?></h2>
 
 <?php BsWp::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
