7d50333dd0f25a2ae0e664e985e949044c5767c1
Francisco J Calderon <fjavier5152@gmail.com>
2018-06-11 22:01:41 -0400

    Update footer.php

--- parts/shared/footer.php
+++ parts/shared/footer.php
@@ -1,4 +1,70 @@
 	
-<div class="footer">
-	
-</div>
+<footer class="footer-component">
+    <div _ngcontent-c2="" class="sj-footer__container">
+        <div _ngcontent-c2="" class="sj-footer__col">
+            <img _ngcontent-c2="" class="sj-footer__logo" src="https://scholarjet.com/assets/images/ScholarJet_Logo_Primary_White_RGB.png">
+            <div _ngcontent-c2="" class="sj-footer__social-container">
+                <a _ngcontent-c2="" class="nodecoration" href="https://www.instagram.com/scholarjet/" rel="noopener noreferrer" target="_blank">
+                    <img _ngcontent-c2="" class="sj-footer__social-icon" src="https://scholarjet.com/assets/images/insta.png">
+                </a>
+                <a _ngcontent-c2="" class="nodecoration" href="https://www.linkedin.com/company/scholarjet?trk=company_logo" rel="noopener noreferrer" target="_blank">
+                    <img _ngcontent-c2="" class="sj-footer__social-icon" src="https://scholarjet.com/assets/images/li.png">
+                </a>
+                <a _ngcontent-c2="" class="nodecoration" href="https://www.facebook.com/ScholarJet-1103038123062741/?fref=ts" rel="noopener noreferrer" target="_blank">
+                    <img _ngcontent-c2="" class="sj-footer__social-icon" src="https://scholarjet.com/assets/images/fb.png">
+                </a>
+                <a _ngcontent-c2="" class="nodecoration" href="https://twitter.com/ScholarJet" rel="noopener noreferrer" target="_blank">
+                    <img _ngcontent-c2="" class="sj-footer__social-icon" src="https://scholarjet.com/assets/images/twitter.png">
+                </a>
+                <a _ngcontent-c2="" class="nodecoration" href="https://medium.com/scholarjet" rel="noopener noreferrer" target="_blank">
+                    <img _ngcontent-c2="" class="sj-footer__social-icon" src="https://scholarjet.com/assets/images/medium.png">
+                </a>
+            </div>
+            <div _ngcontent-c2="" class="sj-footer__link-container">
+                
+                <a _ngcontent-c2="" class="sj-footer__link" href="/how-it-works">How it Works</a>
+                <a _ngcontent-c2="" class="sj-footer__link active" routerlinkactive="active" href="/about-us/team">
+                    Team
+                </a>
+                <a _ngcontent-c2="" class="sj-footer__link" href="https://medium.com/scholarjet" rel="noopener noreferrer" target="_blank">
+                    Blog
+                </a>
+                <a _ngcontent-c2="" class="sj-footer__link" routerlinkactive="active" href="/about-us/privacy-policy">
+                    Privacy Policy
+                </a>
+                <a _ngcontent-c2="" class="sj-footer__link" routerlinkactive="active" href="/about-us/terms-of-use">
+                    Terms of Use
+                </a>
+            </div>
+            <p _ngcontent-c2="" class="sj-footer__copyright">© 2018 ScholarJet. All Rights Reserved.</p>
+            <p _ngcontent-c2="" class="sj-footer__scout">Designed and developed with help from
+                <a _ngcontent-c2="" href="https://web.northeastern.edu/scout/" target="_blank">
+                    <img _ngcontent-c2="" src="https://scholarjet.com/assets/images/scout-footer.png">
+                </a>
+            </p>
+        </div>
+        <div _ngcontent-c2="" class="sj-footer__col">
+            <p _ngcontent-c2="">Have a question or comment? Email us!</p>
+            <a _ngcontent-c2="" class="btn sj-footer__contact-btn" href="mailto:contact@ScholarJet.com?Subject=SJW">contact@ScholarJet.com</a>
+        </div>
+    </div>
+    <!----><div _ngcontent-c2="" class="row ng-star-inserted">
+        <div _ngcontent-c2="" class="text-center">
+            <small _ngcontent-c2="" class="text-muted">v1.3.0</small>
+            <div _ngcontent-c2="" hidden="">
+                <p _ngcontent-c2="">Version information</p>
+                <p _ngcontent-c2="">Front-end</p>
+                <code _ngcontent-c2="">{
+  "build": "36",
+  "tag": null,
+  "longName": "1.3.0",
+  "timestamp": "2018-06-02T17:36:34.244Z",
+  "major": "1",
+  "minor": "3",
+  "patch": "0"
+}</code>
+                <!---->
+            </div>
+        </div>
+    </div>
+</footer>
