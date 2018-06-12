f7aaf2cd10bb527b4585fd383a19bf44417d347c
Francisco J Calderon <fjavier5152@gmail.com>
2018-06-11 21:37:25 -0400

    Update site.js

--- js/site.js
+++ js/site.js
@@ -2,8 +2,8 @@
 jQuery(document).ready(function($) {
 
 	// Your JavaScript goes here
-    $('#sjVideo').on('hidden.bs.modal', function() {
-    const html5Video = document.getElementById("htmlVideo");
+    $('#sjVideModal').on('hidden.bs.modal', function() {
+    const html5Video = document.getElementById("sjVideo");
     if (html5Video != null) {
       html5Video.pause();
       html5Video.currentTime = 0;
