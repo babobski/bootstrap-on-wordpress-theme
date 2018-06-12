ba222b9a2d39c5a64fc49f6c869b622bd261a64c
Francisco J Calderon <fjavier5152@gmail.com>
2018-06-11 21:35:45 -0400

    Update site.js

--- js/site.js
+++ js/site.js
@@ -2,6 +2,13 @@
 jQuery(document).ready(function($) {
 
 	// Your JavaScript goes here
+    $('#sjVideo').on('hidden.bs.modal', function() {
+    const html5Video = document.getElementById("htmlVideo");
+    if (html5Video != null) {
+      html5Video.pause();
+      html5Video.currentTime = 0;
+    }
+  });
 
 });
 
