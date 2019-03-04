d479dfa7c7daadceb7c9946d62b60f53cd5dbd7a
Albert Lee <bertismrpdl@gmail.com>
2019-03-04 17:38:13 -0500

    - Seperated header and footer from content - Gave each section more meaningful class name so that display logic could be easily applicable on wp-admin

--- single.php
+++ single.php
@@ -12,27 +12,35 @@
 <?php BsWp::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-
-	<div class="content">
-	
-		<h2>
+	<div class="wp-post-header">
+		<h2 class="wp-post-title">
 			<?php the_title(); ?>
 		</h2>
-		<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate>
+		<time class="wp-post-timestamp" datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate>
 			<?php the_date(); ?> <?php the_time(); ?>
-		</time> <?php comments_popup_link(__('Leave a Comment', 'wp_babobski'), __('1 Comment', 'wp_babobski'), __('% Comments', 'wp_babobski')); ?>
+		</time> 
+		<span class="wp-post-comment-popup-link">
+			<?php comments_popup_link(__('Leave a Comment', 'wp_babobski'), __('1 Comment', 'wp_babobski'), __('% Comments', 'wp_babobski')); ?>
+		</span>
+	</div>
+
+	<div class="content">
 		<?php the_content(); ?>			
-	
-		<?php if ( get_the_author_meta( 'description' ) ) : ?>
-			<?php echo get_avatar( get_the_author_meta( 'user_email' ) ); ?>
-			<h3>
-				<?php echo __('About', 'wp_babobski'); ?> <?php echo get_the_author() ; ?>
-			</h3>
-			<?php the_author_meta( 'description' ); ?>
-		<?php endif; ?>
-	
-		<?php comments_template( '', true ); ?>
-	
+	</div>
+
+	<div class="wp-post-footer">
+		<div class="wp-post-author">
+			<?php if ( get_the_author_meta( 'description' ) ) : ?>
+				<?php echo get_avatar( get_the_author_meta( 'user_email' ) ); ?>
+				<h3>
+					<?php echo __('About', 'wp_babobski'); ?> <?php echo get_the_author() ; ?>
+				</h3>
+				<?php the_author_meta( 'description' ); ?>
+			<?php endif; ?>
+	    </div>
+	    <div class="wp-post-comment">
+			<?php comments_template( '', true ); ?>
+		</div>
 	</div>
 <?php endwhile; ?>
 
